dd07a3318790e498d0ff24005a5b1f1f76b33207
SoureCopy <hello.fan.andrew@gmail.com>
2016-10-13 18:04:34 +0300

    Update 2nd homework

--- wsgi/main.py
+++ wsgi/main.py
@@ -1,10 +1,5 @@
 from wsgiref.simple_server import make_server
 from wsgiClass import WSGIapp
-import requests
 
 http_server = make_server('localhost', 35235, WSGIapp, )
 http_server.serve_forever()
-for elem in range(5):
-    response = requests.get('http://localhost:35235/')
-    print(response.status_code)
-    print(response.text)
