c39284fd54362130053930df0cf899bb885f47da
SoureCopy <hello.fan.andrew@gmail.com>
2016-10-20 18:23:09 +0300

    Delete files and directories

--- wsgi/main.py
+++
@@ -1,5 +0,0 @@
-from wsgiref.simple_server import make_server
-from wsgiClass import WSGIapp
-
-http_server = make_server('localhost', 35235, WSGIapp, )
-http_server.serve_forever()
--- wsgi/requestTest.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-for elem in range(5):
-    response = requests.get('http://localhost:35235/')
-    print(response.status_code)
-    print(response.text)
